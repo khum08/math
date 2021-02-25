60c6aff4fefadca8325dae05265552a23e168555
yuanzhenkun <yuanzhenkun@baidu.com>
2021-02-26 00:30:50 +0800

    feat: add MathUtils

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,2 +1,33 @@
-public class MathUtils implements IMathUtils {
+public class MathUtils {
+
+    static final double EPSILON = 1e-8;
+
+    /**
+     * radian transform to degree.
+     * @param rad radian value
+     * @return degree value
+     */
+    public double rad2deg(double rad) {
+        return rad * 180 / Math.PI;
+    }
+
+    /**
+     * degree transform to radian.
+     * @param deg degree value.
+     * @return radian value.
+     */
+    public double deg2rad(double deg) {
+        return deg * Math.PI / 180;
+    }
+
+    /**
+     * clamp the x to be between min and max.
+     * @param x value to be clamped.
+     * @param min minimum.
+     * @param max maximum.
+     * @return result.
+     */
+    public double clamp(double x, double min, double max) {
+        return Math.max(min, Math.min(x, max));
+    }
 }
