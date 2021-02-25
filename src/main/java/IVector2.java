30c0f977d55298f4cb267b9025686eddd413ac3e
yuanzhenkun <yuanzhenkun@baidu.com>
2021-02-25 20:59:47 +0800

    feat: add vec2

--- src/main/java/IVector2.java
+++ src/main/java/IVector2.java
@@ -1,2 +1,17 @@
 public interface IVector2 {
+    double x = 0;
+    double y = 0;
+
+    IVector2 add(IVector2 v);
+    IVector2 sub(IVector2 v);
+    IVector2 scale(double scalar);
+
+    double dot(IVector2 v);
+    double cross(IVector2 v);
+
+    double length();
+
+    IVector2 rotate(double rad);
+
+
 }
--- src/main/java/Vector2.java
+++ src/main/java/Vector2.java
@@ -1,2 +1,51 @@
 public class Vector2 implements IVector2 {
+
+    double x;
+    double y;
+
+    public Vector2() {
+    }
+
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public IVector2 add(IVector2 v) {
+        x += v.x;
+        y += v.y;
+        return this;
+    }
+
+    public IVector2 sub(IVector2 v) {
+        x -= v.x;
+        y -= v.y;
+
+        return this;
+    }
+
+    public IVector2 scale(double scalar) {
+        x *= scalar;
+        y *= scalar;
+
+        return this;
+    }
+
+    public double dot(IVector2 v) {
+        return x * v.x + y * v.y;
+    }
+
+    public double cross(IVector2 v) {
+        return x * v.y - y * v.x;
+    }
+
+    public double length() {
+        return Math.hypot(x, y);
+    }
+
+    public IVector2 rotate(double rad) {
+        x = x * Math.cos(rad) - y * Math.sin(rad);
+        y = x * Math.sin(rad) + y * Math.cos(rad);
+        return this;
+    }
 }
